c7bd638ea2052f872c9db937a5c101259fca7622
Fabrício Terra <lima.de.terra@hotmail.com>
2022-04-27 16:43:55 -0300

    Criação das classes sem as ligações

---
+++ EssentialConnection/EssentialConnection/Models/Curriculo.cs
@@ -0,0 +1,9 @@
+﻿namespace EssentialConnection.Models
+{
+    public class Curriculo
+    {
+        public int CurriculoID { get; set; }
+        public string DescricaoPessoal { get; set; }
+        
+    }
+}
---
+++ EssentialConnection/EssentialConnection/Models/Empresa.cs
@@ -0,0 +1,12 @@
+﻿namespace EssentialConnection.Models
+{
+    public class Empresa
+    {
+        public int EmpresaID { get; set; }
+        public string Nome { get; set; }
+        public string CNPJ { get; set; }
+        public string Telefone { get; set; }
+        public string NomeResponsavel { get; set; }
+        public string Descricao { get; set; }
+    }
+}
