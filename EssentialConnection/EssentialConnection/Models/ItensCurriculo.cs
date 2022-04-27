94d0851f5ddd0fe43e1710ef74ce072c2e44e36c
Thiago-Faculdade <75047728+Thiago-Faculdade@users.noreply.github.com>
2022-04-27 20:46:57 +0000

    Update ItensCurriculo.cs

--- EssentialConnection/EssentialConnection/Models/ItensCurriculo.cs
+++ EssentialConnection/EssentialConnection/Models/ItensCurriculo.cs
@@ -9,7 +9,7 @@
         public DateTime DataInicio { get; set; }
         public DateTime DataFim { get; set; }
         public string Instituicao { get; set; }
-        public ICollection<Curriculo> Curriculos { get; set; }
+        public Curriculo Curriculo { get; set; }
 
     }
 }
