a41a4b5fd1224b29454ddc1b6d7397c7068299a8
Fabrício Terra <lima.de.terra@hotmail.com>
2022-04-27 19:48:09 -0300

    Commit com o comentario

--- EssentialConnection/EssentialConnection/Models/Curriculo.cs
+++ EssentialConnection/EssentialConnection/Models/Curriculo.cs
@@ -8,7 +8,7 @@ namespace EssentialConnection.Models
     public class Curriculo
     {
         public int CurriculoID { get; set; }
-        public string DescricaoPessoal { get; set; } //testando 
+        public string DescricaoPessoal { get; set; } //testando denovo
         
     }
 }
