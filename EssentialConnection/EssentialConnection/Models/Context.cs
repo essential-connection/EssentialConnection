22f5de6c7049843dfec3ef0cc210b9dc30e30965
Thiago-Faculdade <75047728+Thiago-Faculdade@users.noreply.github.com>
2022-04-30 15:36:12 +0000

    Context
    eu fiz essa classe que fala de todas as tabelas

---
+++ EssentialConnection/EssentialConnection/Models/Context.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Atividade_Intregadora.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Curriculo> Curriculos { get; set; }
+
+        public DbSet<Curso> Curso { get; set; }
+        public DbSet<Empresa> Empresas { get; set; }
+        public DbSet<Vagas> Vagas { get; set; }
+
+        public DbSet<Competencias> Competencias { get; set; }
+        
+        public DbSet<ItensCurriculo> ItensCurriculos { get; set; }
+
+    }
+}
+
