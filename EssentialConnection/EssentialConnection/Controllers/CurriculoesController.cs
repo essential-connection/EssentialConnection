9e3c33d47450903533b009c47c62f94df2747b69
Fabrício Terra <lima.de.terra@hotmail.com>
2022-05-03 17:11:08 -0300

    Criação do Curriculo Controller com as views
    Criação do controller de curriculo com as views incluidas

---
+++ EssentialConnection/EssentialConnection/Controllers/CurriculoesController.cs
@@ -0,0 +1,160 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EssentialConnection.Models;
+
+namespace EssentialConnection.Controllers
+{
+    public class CurriculoesController : Controller
+    {
+        private readonly Context _context;
+
+        public CurriculoesController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Curriculoes
+        public async Task<IActionResult> Index()
+        {
+            var context = _context.Curriculo.Include(c => c.Aluno);
+            return View(await context.ToListAsync());
+        }
+
+        // GET: Curriculoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var curriculo = await _context.Curriculo
+                .Include(c => c.Aluno)
+                .FirstOrDefaultAsync(m => m.CurriculoID == id);
+            if (curriculo == null)
+            {
+                return NotFound();
+            }
+
+            return View(curriculo);
+        }
+
+        // GET: Curriculoes/Create
+        public IActionResult Create()
+        {
+            ViewData["CurriculoID"] = new SelectList(_context.Aluno, "AlunoID", "AlunoID");
+            return View();
+        }
+
+        // POST: Curriculoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CurriculoID,DescricaoPessoal,AlunoId")] Curriculo curriculo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(curriculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CurriculoID"] = new SelectList(_context.Aluno, "AlunoID", "AlunoID", curriculo.CurriculoID);
+            return View(curriculo);
+        }
+
+        // GET: Curriculoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var curriculo = await _context.Curriculo.FindAsync(id);
+            if (curriculo == null)
+            {
+                return NotFound();
+            }
+            ViewData["CurriculoID"] = new SelectList(_context.Aluno, "AlunoID", "AlunoID", curriculo.CurriculoID);
+            return View(curriculo);
+        }
+
+        // POST: Curriculoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CurriculoID,DescricaoPessoal,AlunoId")] Curriculo curriculo)
+        {
+            if (id != curriculo.CurriculoID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(curriculo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CurriculoExists(curriculo.CurriculoID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CurriculoID"] = new SelectList(_context.Aluno, "AlunoID", "AlunoID", curriculo.CurriculoID);
+            return View(curriculo);
+        }
+
+        // GET: Curriculoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var curriculo = await _context.Curriculo
+                .Include(c => c.Aluno)
+                .FirstOrDefaultAsync(m => m.CurriculoID == id);
+            if (curriculo == null)
+            {
+                return NotFound();
+            }
+
+            return View(curriculo);
+        }
+
+        // POST: Curriculoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var curriculo = await _context.Curriculo.FindAsync(id);
+            _context.Curriculo.Remove(curriculo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CurriculoExists(int id)
+        {
+            return _context.Curriculo.Any(e => e.CurriculoID == id);
+        }
+    }
+}
