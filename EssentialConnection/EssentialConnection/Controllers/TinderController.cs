53d11948b6b721f6136386382651578638c8e7fb
Gustavo Luiz <glogustavo4321@gmail.com>
2022-05-14 17:40:27 -0300

    Pssando pro outro pc

---
+++ EssentialConnection/EssentialConnection/Controllers/TinderController.cs
@@ -0,0 +1,22 @@
+﻿using EssentialConnection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using X.PagedList;
+
+namespace EssentialConnection.Controllers
+{
+    public class TinderController : Controller
+    {
+        private readonly Context _context;
+
+        public TinderController(Context context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int pagina = 1)
+        {
+            var context = _context.Vaga.Include(v => v.Curso).Include(v => v.Empresa);
+            return View((await context.ToListAsync()).ToPagedList(pagina,1));
+        }
+    }
+}
