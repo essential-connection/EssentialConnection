d9bf9e2b964ee8fd32c3bf07069db53d91ed9f72
Gustavo Luiz <glogustavo4321@gmail.com>
2022-05-15 01:22:36 -0300

    Alterações Qualquers

--- EssentialConnection/EssentialConnection/Controllers/TinderController.cs
+++ EssentialConnection/EssentialConnection/Controllers/TinderController.cs
@@ -2,13 +2,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using X.PagedList;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace EssentialConnection.Controllers
 {
+    [Authorize]
     public class TinderController : Controller
     {
         private readonly Context _context;
-
         public TinderController(Context context)
         {
             _context = context;
@@ -17,6 +19,13 @@ namespace EssentialConnection.Controllers
         {
             var context = _context.Vaga.Include(v => v.Curso).Include(v => v.Empresa);
             return View((await context.ToListAsync()).ToPagedList(pagina,1));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(int vagaId,string nomeVaga)
+        {
+            
+            return View();
         }
     }
 }
---
+++ EssentialConnection/EssentialConnection/Models/Tinder.cs
@@ -0,0 +1,9 @@
+﻿namespace EssentialConnection.Models
+{
+    public class Tinder
+    {
+        public int Id { get; set; }
+        public string AlunoId { get; set; }
+        public int VagaId { get; set; }
+    }
+}
