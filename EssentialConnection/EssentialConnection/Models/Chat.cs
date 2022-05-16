1b2f99f2cf8e53e373a33239c6683df201a8c425
Thiago-Faculdade <75047728+Thiago-Faculdade@users.noreply.github.com>
2022-05-16 21:00:13 +0000

    classes chat e usuario

---
+++ EssentialConnection/EssentialConnection/Models/Chat.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EssentialConnection.Models
+{
+    public class Chat
+    {
+        public int ChatId { get; set; }
+        [Required]
+        public string? NomeAluno { get; set; }
+        [Required]
+        public string? Mensagem { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+
+        public int UsuarioID { get; set; }
+
+        public virtual Usuario Usuario { get; set; }
+
+    }
+}
---
+++ EssentialConnection/EssentialConnection/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EssentialConnection.Models
+{
+    public class Usuario : IdentityUser
+    {
+        public Usuario()
+        {
+            Chats = new HashSet<Chat>();
+        }
+        public virtual ICollection<Chat> Chats { get; set; }
+    }
+}
