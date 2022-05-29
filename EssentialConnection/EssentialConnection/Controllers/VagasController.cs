83a33cfcd479e82830eda4af1d34ef65688166f0
Gustavo Luiz <glogustavo4321@gmail.com>
2022-05-29 01:15:38 -0300

    Vagas resolvido-parte-criacao

--- EssentialConnection/EssentialConnection/Controllers/VagasController.cs
+++ EssentialConnection/EssentialConnection/Controllers/VagasController.cs
@@ -7,16 +7,20 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using EssentialConnection.Models;
+using EssentialConnection.Areas.Identity.Data;
+using Microsoft.AspNet.Identity;
 
 namespace EssentialConnection.Controllers
 {
     public class VagasController : Controller
     {
         private readonly Context _context;
+        private readonly IdentityContext _identityContext;
 
-        public VagasController(Context context)
+        public VagasController(Context context, IdentityContext identityContext)
         {
             _context = context;
+            _identityContext = identityContext;
         }
 
         // GET: Vagas
@@ -49,8 +53,6 @@ namespace EssentialConnection.Controllers
         // GET: Vagas/Create
         public IActionResult Create()
         {
-            ViewData["CursoId"] = new SelectList(_context.Curso.OrderBy(c=>c.Nome), "CursoID", "Nome");
-            ViewData["EmpresaId"] = new SelectList(_context.Empresa.OrderBy(e=>e.Nome), "EmpresaID", "Nome");
             return View();
         }
 
@@ -59,8 +61,23 @@ namespace EssentialConnection.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("VagaID,Nome,Descricao,Quantidade,DataExpiracao,Status,Responsavel,EmpresaId,CursoId")] Vaga vaga)
+        public async Task<IActionResult> Create([Bind("VagaID,Nome,Descricao,Quantidade,DataExpiracao,Status,Responsavel")] Vaga vaga)
         {
+            //PODIA TER FEITO ESSA LÓGICA MAIS SIMPLES, MAS PRO PROVO ENTENDER SE PRECISAR EXPLICAR
+            //OBSERVAÇÃO
+            var userLogado = _identityContext.Users.FirstOrDefault(u => u.Id == User.Identity.GetUserId());
+            if (userLogado.Tipo == EssentialConnectionUser.TipoUsuario.Professor)
+            {
+                var CursoLogado=_context.Curso.FirstOrDefault(x=>x.UserId == User.Identity.GetUserId());
+                vaga.CursoId = CursoLogado.CursoID;
+                vaga.EmpresaId = null;
+            }
+            else if (userLogado.Tipo == EssentialConnectionUser.TipoUsuario.Empresa)
+            {
+                var EmpresaLogada = _context.Empresa.FirstOrDefault(e => e.UserId == User.Identity.GetUserId());
+                vaga.EmpresaId = EmpresaLogada.EmpresaID;
+                vaga.CursoId = null;
+            }
             _context.Add(vaga);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
